df34d0411db4ad3b3eeb5210b7dc78945870bc8f
alterego-13 <57637036+alterego-13@users.noreply.github.com>
2019-11-13 17:58:08 +0300

    create some function

--- src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,12 +1,27 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {
-        String somebody ="World!";
-        System.out.println("Hello " + somebody);
-        int l =6;
-        int s=l*l;
-        System.out.println("площадь квадрата со стороной " + l + " = "+ s );
-    }
+  public static void main(String[] args) {
+    hello("world");
+    double len = 5;
+    System.out.println("Площадь квадрата со стороной " + len + "=" + area(len));
+double a = 4 ;
+double b = 6 ;
+    System.out.println("площадь прям со сторонами"+ a + "="+ b + "=" + area(a,b));
+  }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello  " + somebody);
+  }
+
+  public static double area(double l) {
+    return l * l;
+
+  }
+
+  public static double area(double a, double b){
+    return a * b ;
+  }
 
 }
