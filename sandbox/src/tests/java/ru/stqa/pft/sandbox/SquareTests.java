1027ec39cc641835e9b2984679678d125deb24db
alterego-13 <57637036+alterego-13@users.noreply.github.com>
2019-11-20 18:06:47 +0300

    create SquareTests

--- sandbox/src/tests/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/tests/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -1,10 +1,12 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.annotations.Test;
+
 /**
  * Created by Андрей on 20.11.2019.
  */
 public class SquareTests {
-
+@Test
   public void testArea(){
     Square s = new Square(5);
     assert s.area()==25;
