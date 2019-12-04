c9d7de5098fa44f805bfd31ba6a1d46ba2c9daee
alterego-13 <57637036+alterego-13@users.noreply.github.com>
2019-12-04 18:51:07 +0300

    create group delete test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreateTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreateTest.java
@@ -1,127 +1,18 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.concurrent.TimeUnit;
+import org.testng.annotations.Test;
 
-import org.testng.annotations.*;
-
-import static org.testng.Assert.*;
-
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-public class GroupCreateTest {
-  private WebDriver driver;
-  public String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
-
-
-  @BeforeClass(alwaysRun = true)
-  public void setUp() throws Exception {
-    final String s = System.setProperty("webdriver.gecko.driver", "C://Program Files//Mozilla Firefox/geckodriver.exe");
-    driver = new FirefoxDriver();
-    baseUrl = "http://www.google.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    driver.get("http://localhost/addressbook/");
-    login("admin","secret");
-
-  }
-
-
-
-  private void login(String username, String password) {
-
-    driver.findElement(By.xpath("//img[@alt='Address book']")).click();
-    driver.findElement(By.id("content")).click();
-    driver.findElement(By.id("LoginForm")).click();
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys(username);
-    driver.findElement(By.xpath("//*/text()[normalize-space(.)='']/parent::*")).click();
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys(password);
-    driver.findElement(By.xpath("//input[@value='Login']")).click();
-  }
+public class GroupCreateTest extends TestBase {
 
 
   @Test
   public void testCreateGroupCase() throws Exception {
 
-    gotoGroupPage();
+    returnToGroupPage();
     initGroupCreation();
     fillGroupForm(new GroupData("test1", "test2", "test3"));
     submitGroupCreation();
     returnToGroupPage();
   }
 
-  private void returnToGroupPage() {
-    gotoGroupPage();
-  }
-
-  private void submitGroupCreation() {
-    driver.findElement(By.name("submit")).click();
-  }
-
-  private void fillGroupForm(GroupData groupData) {
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  private void initGroupCreation() {
-    driver.findElement(By.name("new")).click();
-  }
-
-  private void gotoGroupPage() {
-    driver.findElement(By.linkText("groups")).click();
-  }
-
-  @AfterClass(alwaysRun = true)
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
 }
