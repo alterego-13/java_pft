8bfc7b36e6e0a2b05b0d5b014a41b9dd386075b9
alterego-13 <57637036+alterego-13@users.noreply.github.com>
2019-12-18 00:59:07 +0300

    creaye groupCache
    кэщировние резултатов добавление gropCache в GroupHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -44,11 +44,11 @@ public class GroupHelper extends HelperBase {
   }
 
 
- /* public void selectGroup(int index) {
-    driver.findElements
-            (By.name("selected[]")).get(index).click();
-  }
-*/
+  /* public void selectGroup(int index) {
+     driver.findElements
+             (By.name("selected[]")).get(index).click();
+   }
+ */
   public void selectGroupById(int id) {
     driver.findElement
             (By.cssSelector("input[value='" + id + "']")).click();
@@ -70,22 +70,24 @@ public class GroupHelper extends HelperBase {
     initGroupCreation();
     fillGroupForm(group);
     submitGroupCreation();
+    groupCache = null;
     returnToGroupPage();
   }
 
-  public void modify( GroupData group) {
+  public void modify(GroupData group) {
     selectGroupById(group.getId());
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
+    groupCache = null;
     returnToGroupPage();
   }
-
 
 
   public void delete(GroupData group) {
     selectGroupById(group.getId());
     deleteSelectedGroups();
+    groupCache = null;
     returnToGroupPage();
   }
 
@@ -98,28 +100,33 @@ public class GroupHelper extends HelperBase {
     return driver.findElements(By.name("selected[]")).size();
   }
 
- /* public List<GroupData> list() {
-    List<GroupData> groups = new ArrayList<GroupData>();
-    List<WebElement> elements = driver.findElements(By.cssSelector("span.group"));
-    for (WebElement element : elements) {
-      String name = element.getText();
-      int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
+  /* public List<GroupData> list() {
+     List<GroupData> groups = new ArrayList<GroupData>();
+     List<WebElement> elements = driver.findElements(By.cssSelector("span.group"));
+     for (WebElement element : elements) {
+       String name = element.getText();
+       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
 
-      groups.add(new GroupData().withId(id).withName(name));
-    }
-    return groups;
-  }
-*/
+       groups.add(new GroupData().withId(id).withName(name));
+     }
+     return groups;
+   }
+ */
+  private Groups groupCache = null;
+
   public Groups all() {
-    Groups groups = new Groups();
+    if (groupCache != null) {
+      return new Groups(groupCache);
+    }
+    groupCache = new Groups();
     List<WebElement> elements = driver.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
       String name = element.getText();
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
 
-      groups.add(new GroupData().withId(id).withName(name));
+      groupCache.add(new GroupData().withId(id).withName(name));
     }
-    return groups;
+    return new Groups(groupCache);
   }
 }
 
