4e8f55b9b9342a8bd901b8a4c5d53c8d1b8cad19
alterego-13 <57637036+alterego-13@users.noreply.github.com>
2020-01-10 01:15:58 +0300

    Create Logback
    создание логов. запись в тхт файл и вывод в консоль

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreateTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreateTest.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -21,25 +23,27 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreateTest extends TestBase {
 
+
   @DataProvider
   public Iterator<Object[]> validGroupsFromXml() throws IOException {
 
-   try ( BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))){
-    String xml = "";
-    String line = reader.readLine();
-    while (line != null) {
-      xml += line;
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))) {
+      String xml = "";
+      String line = reader.readLine();
+      while (line != null) {
+        xml += line;
+        line = reader.readLine();
 
-      // String[] split = line.split(";");
-      // list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+        // String[] split = line.split(";");
+        // list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
 
+      }
+      XStream xstream = new XStream();
+      xstream.processAnnotations(GroupData.class);
+      List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+      return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
-    XStream xstream = new XStream();
-    xstream.processAnnotations(GroupData.class);
-    List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
-    return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
-  }}
+  }
     /* создание данных
     list.add(new Object[]{new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
     list.add(new Object[]{new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
@@ -68,6 +72,7 @@ public class GroupCreateTest extends TestBase {
   @Test(dataProvider = "validGroupsFromJson")
   public void testCreateGroupCase(GroupData group) {
 
+
     // GroupData group = new GroupData().withName(name).withHeader(header).withFooter(footer);
     app.group().returnToGroupPage();
     Groups before = app.group().all();
@@ -76,6 +81,8 @@ public class GroupCreateTest extends TestBase {
     Groups after = app.group().all();
     assertThat(after, equalTo
             (before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
+
   }
 
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,14 +1,22 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 import static org.testng.Assert.fail;
 
 
 public class TestBase {
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
@@ -27,4 +35,14 @@ public class TestBase {
     }*/
   }
 
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p ) {
+    logger.info("Start test" + m.getName() + " with parameters" + Arrays.asList(p));
+
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m) {
+    logger.info("Stop test" + m.getName());
+  }
 }
